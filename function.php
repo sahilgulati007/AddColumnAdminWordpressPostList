d68082d80192164ea820eaf3462df11155924554
Sahil Gulati <sahilgulati49@gmail.com>
2020-05-25 10:57:02 +0530

    Added cpt cpde

--- function.php
+++ function.php
@@ -12,3 +12,19 @@ function wpdocs_posts_custom_columns( $column_name, $id ) {
     }
 }
 add_action( 'manage_posts_custom_column', 'wpdocs_posts_custom_columns', 5, 2 );
+
+//pages
+function add_sticky_column_page( $columns ) {
+	$columns['pdf'] = __('PDF');
+        return $columns;
+}
+add_filter( 'manage_page_posts_columns' , 'add_sticky_column_page' );
+
+function wpdocs_page_posts_custom_columns( $column_name, $id ) {
+    if ( 'pdf' === $column_name ) {
+        echo $id;
+    }
+}
+add_action( 'manage_page_posts_custom_column', 'wpdocs_page_posts_custom_columns', 5, 2 );
+
+//cpt -> manage_{posttype}_posts_custom_column, manage_{posttype}_posts_columns
